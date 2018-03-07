23eea8fe938bcf8a58e06dd264e4e087536b04de
Christiano Milfont <cmilfont@gmail.com>
2018-03-07 09:01:43 -0300

    zerando

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Activities from './components/activities';
 
 const result = [
   {
@@ -15,42 +14,11 @@ const result = [
 
 class App extends React.Component {
 
-  static childContextTypes = {
-    edit: PropTypes.func,
-  }
-
-  state = {
-    activities: [],
-    activity: {},
-  }
-
-  getChildContext() {
-    return {
-      edit: this.edit
-    }
-  }
-
-  componentDidMount() {
-    // fetch('/activities.json').then(result)
-    this.setState({
-      activities: result
-    });
-  }
-  
-  edit = (activity) => {
-    this.setState({
-      ...this.state,
-      activity,
-    })
-  }
 
   render() {
 
     return (
-      <Activities
-        activity={this.state.activity}
-        activities={this.state.activities}
-      />
+      <div>Ola CVC</div>
     );
   }
 
